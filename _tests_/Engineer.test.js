6f025312751767a8e510c60ec479cc32579c96f4
Mystic Kitty <djabranton@gmail.com>
2020-11-20 10:34:23 -0800

    Created Engineer test - failed/passed.  Created all employee classes, extended out from employee and included super keyword in constructors.

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,37 @@
+const Engineer = require('../lib/Engineer.js')
+
+describe('When an engineer object is created', () => {
+    let sammy;
+    beforeEach(() => {
+        sammy = new Engineer('Sammy Sossa', 2468, 'sam@samster.com', 'sammysgit')
+    });
+    test('Create an engineer Object', () => {
+        expect(sammy.name).toEqual(expect.any(String));
+        expect(sammy.id).toEqual(expect.any(Number));
+        expect(sammy.email).toBe('sam@samster.com');
+        expect(sammy.github).toBe('sammysgit');
+    })
+
+    describe('When an engineer github is created', () => {
+        let sammy;
+        beforeEach(() => {
+            sammy = new Engineer('Sammy Sossa', 2468, 'sam@samster.com', 'sammysgit')
+        });
+        test('Get engineer github', () => {
+            expect(sammy.getGithub()).toEqual(expect.stringContaining('sammysgit'));
+        })
+    })
+
+    describe('When an engineer role is created', () => {
+        let sammy;
+        beforeEach(() => {
+            sammy = new Engineer('Sammy Sossa', 2468, 'sam@samster.com', 'sammysgit')
+        });
+        test('Get engineer role', () => {
+            expect(sammy.getRole()).toEqual(expect.stringContaining('Engineer'));
+        })
+    })
+})
+
+
+
